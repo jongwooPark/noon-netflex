d644a77e3bb4ddf7350d527686308b0a6561a2f1
jongwooPark <jongwoo4u@gmail.com>
2023-01-12 17:56:22 +0900

    스피너

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,17 +5,17 @@ import { movieAction } from "../redux/actions/movieAction";
 import { useDispatch, useSelector } from "react-redux";
 import Banner from "../components/Banner";
 import MovieSlide from "../components/MovieSlide";
-import ClipLoader from "react-spinners/ClipLoader";
+import ClipLoader from "react-spinners/SyncLoader";
 import { useState, CSSProperties } from "react";
 
 
 const Home = () => {
 
-    let [loading2, setLoading2] = useState(true);
-    let [color, setColor] = useState("#ffffff");
+  let [loading2, setLoading2] = useState(true);
+  let [color, setColor] = useState("#ffffff");
   const dispatch = useDispatch();
 
-  const { popularMovies, topMovies, upcommingMovies , loading} = useSelector(
+  const { popularMovies, topMovies, upcommingMovies, loading } = useSelector(
     (state) => state.movie
   );
 
@@ -27,40 +27,22 @@ const Home = () => {
 
   }, []);
 
- if(loading){
-    return
-    <ClipLoader
-    color="red"
-    loading={loading}
-   
-    size={150}
-    aria-label="Loading Spinner"
-    data-testid="loader"
-  
-  />
-}
-
-   return (
-     <div>
-    <ClipLoader
-    color= "red"
-    loading2={loading2}
-   
-    size={150}
-    aria-label="Loading Spinner"
-    data-testid="loader"
-  
-  />
-
-       {topMovies.results && <Banner movie={popularMovies.results[0]} />}
-       <h1>Popular Movies</h1>
-       <MovieSlide movies={popularMovies} />
-       <h1>Top Movies</h1>
-       <MovieSlide movies={topMovies} />
-       <h1>Upcomming Movies</h1>
-       <MovieSlide movies={upcommingMovies} />
-     </div>
-   );
+  if (loading) return (
+    <div>
+      <ClipLoader color="red" loading={loading} size={150} aria-label="Loading Spinner" data-testid="loader" />
+    </div>
+  ) 
+  return (
+    <div>
+      {topMovies.results && <Banner movie={popularMovies.results[0]} />}
+      <h1>Popular Movies</h1>
+      <MovieSlide movies={popularMovies} />
+      <h1>Top Movies</h1>
+      <MovieSlide movies={topMovies} />
+      <h1>Upcomming Movies</h1>
+      <MovieSlide movies={upcommingMovies} />
+    </div>
+  );
 };
 
 export default Home;
